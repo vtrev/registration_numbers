18b9fd19973850e58d71d095755e684e320c4aa8
vusi <baloyi.trev@gmail.com>
2018-05-15 09:58:18 +0200

    added some functions for the filter,restructured code to accomodate filters

--- registration_number.js
+++ registration_number.js
@@ -14,31 +14,96 @@ var checkReg = function () {
 var addNumberPlatesFactory = function(platesArray){
     
     var tmpPlates = platesArray;
-    console.log(tmpPlates);
+    //console.log(tmpPlates);
     var addPlateElement = function(plate){
+        plate = plate.toUpperCase();
         var listItem = document.createElement("li");
         var plateText = document.createTextNode(plate);
         listItem.appendChild(plateText);
         document.getElementById('numberPlates').appendChild(listItem);
-        tmpPlates.push(plateText);
+        tmpPlates.push(plate);
         localStorage.setItem('regArray', JSON.stringify(tmpPlates));
         
     }; 
+    
+    
+
+    var filterFunction  =  function(unsortedPlates){
+        var capetown = [];
+        var bellville = [];
+        var stellies = [];
+        var paarl = [];
+
+        for( let i = 0; i < unsortedPlates.length ;  i++ ){
+            if(unsortedPlates[i].startsWith('CA')){
+                capetown.push(unsortedPlates[i]);
+            }
+            if(unsortedPlates[i].startsWith('CY')){
+                bellville.push(unsortedPlates[i]);
+            }
+            if(unsortedPlates[i].startsWith('CL')){
+                stellies.push(unsortedPlates[i]);
+            }
+            if(unsortedPlates[i].startsWith('CJ')){
+                paarl.push(unsortedPlates[i]);
+            }
+
+
+
+            
+        }
+        return {
+            capetown,
+            bellville,
+            stellies,
+            paarl
+        }
+    }
+        
+        return{
+            addPlateElement,
+            filterFunction
+        }
+
+    
+
+
+
+
+}
+
+
+// ===================DOM===================================
+
+var addNumberPlates = function(){
+    var addPlate  = addNumberPlatesFactory(JSON.parse(localStorage.getItem('regArray')));
+
+    checkReg();
+    var plateElement = document.getElementById('inputBox').value;
+    console.log(addPlate);
+    addPlate.addPlateElement(plateElement);
+    
     var clearTextBox = function(){
 
         plateElement.value = "";
-    }
-    return{
-        addPlateElement
-    }
-}
-// ===================DOM===================================
-var addNumberPlates = function(){
-    checkReg();
-    var plateElement = document.getElementById('inputBox');
-    
-    var addPlate  = addNumberPlatesFactory(JSON.parse(localStorage.getItem('userMap')));
-    addPlate.addPlateElement(plateElement.value);
+    }    
     clearTextBox();
     return false;
 }
+
+
+var filterBtn = document.getElementById('filterButton');
+
+filterBtn.addEventListener('click',function(){
+    var addPlate  = addNumberPlatesFactory(JSON.parse(localStorage.getItem('regArray')));
+
+    
+    var dropD = document.getElementById('townDropD');
+    var dropDvalue = dropD.options[dropD.selectedIndex].value
+    var arrayFromCache = JSON.parse(localStorage.getItem('regArray'));    
+    var filter = addPlate.filterFunction(arrayFromCache).dropDvalue;
+    // console.log(dropDvalue);
+    console.log(filter);
+
+}
+);
