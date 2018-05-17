2cc2f4a0864e9a943c1db049efd717ec749e1cc5
vtrev <baloyi.trev@gmail.com>
2018-05-17 19:26:52 +0200

    removed complex filter function,allowed empty space as part of input

--- registration_number.js
+++ registration_number.js
@@ -1,89 +1,50 @@
 // ==================LOGIC==========================
 
-var addNumberPlatesFactory = function(platesArray){
+var addNumberPlatesFactory = function (platesArray) {
 
-    // var platesArray = platesArray;
+    var platesArray = platesArray;
     var tmpPlates = platesArray;
-    var addPlateElement = function(plate){
+    var addPlateElement = function (plate) {
         plate = plate.toUpperCase();
         tmpPlates.push(plate);
         localStorage.setItem('regArray', JSON.stringify(tmpPlates));
-    }; 
-    var filterFunction  =  function(unsortedPlates){
-        var capetown = [];var bellville = [];var stellies = [];var paarl = [];
-    
-        for( let i = 0; i < unsortedPlates.length ;  i++ ){
-            if(unsortedPlates[i].startsWith('CA')){
-                capetown.push(unsortedPlates[i]);
+    };   
+    var filterFunction = function (town) {
+        var unsortedPlates = JSON.parse(localStorage.getItem('regArray'));
+        if (town == 'alltowns') {
+            return unsortedPlates
+        } else {
+            var holdingArray = [];
+            for (let i = 0; i < unsortedPlates.length; i++) {
+                if (unsortedPlates[i].startsWith(town)) {
+                    holdingArray.push(unsortedPlates[i]);
+                }
             }
-            if(unsortedPlates[i].startsWith('CY')){
-                bellville.push(unsortedPlates[i]);
-            }
-            if(unsortedPlates[i].startsWith('CL')){
-                stellies.push(unsortedPlates[i]);
-            }
-            if(unsortedPlates[i].startsWith('CJ')){
-                paarl.push(unsortedPlates[i]);
-            }        
-        }
-        return {
-            capetown,
-            bellville,
-            stellies,
-            paarl
+            return holdingArray
         }
     }
 
-    //filter the stored array and return sorted arrays  based on town
-    var getTown = function(town){
-        var addPlate  = addNumberPlatesFactory(platesArray);
-    
-        var filter = addPlate.filterFunction(platesArray);
-    
-        if(town == 'alltowns'){
-            return JSON.parse(localStorage.getItem('regArray'))
-        }
-        
-        if( town == 'capetown'){
-        return filter.capetown
-        }
-        if( town == 'paarl'){
-            return filter.paarl   
-        
-        }
-        if( town == 'bellville'){
-            return filter.bellville    
-        }
-        if(town == 'stellies'){
-            return filter.stellies
-        }
-        
-    
+    return {
+        addPlateElement,
+        filterFunction
     }
-            
-        return{
-            addPlateElement,
-            filterFunction,
-            getTown
-        }
 
 }
 
-
 // ===================DOM===================================
 
-var displayPlates = function(platesArray){
+var displayPlates = function (platesArray) {
     document.getElementById('numberPlates').innerHTML = '';
     platesArray.reverse();
-for( var i = 0 ; i < platesArray.length ; i++ ){
-    var plate = platesArray[i];
+    for (var i = 0; i < platesArray.length; i++) {
+        var plate = platesArray[i];
         plate = plate.toUpperCase();
         var listItem = document.createElement("li");
-        var plateText =document.createTextNode(plate);
+        var plateText = document.createTextNode(plate);
         listItem.appendChild(plateText);
-        document.getElementById('numberPlates').appendChild(listItem); 
+        document.getElementById('numberPlates').appendChild(listItem);
     }
-    
+
 }
 //check for a redisttration array and create it if null
 var checkReg = function () {
@@ -93,24 +54,22 @@ var checkReg = function () {
 }
 
 //function to run when the add button is clicked
-var addNumberPlates = function(){
+var addNumberPlates = function () {
     checkReg();
-    var addPlate  = addNumberPlatesFactory(JSON.parse(localStorage.getItem('regArray')));
+    var addPlate = addNumberPlatesFactory(JSON.parse(localStorage.getItem('regArray')));
     var plateElement = document.getElementById('inputBox').value;
     addPlate.addPlateElement(plateElement);
-    document.getElementById('inputBox').value= "";
+    document.getElementById('inputBox').value = "";
     displayPlates(JSON.parse(localStorage.getItem('regArray')));
-    
+
     return false
 }
+
 //an event listener for the apply filter button that will run the filter function and display the filtered results
 
-document.getElementById('filterButton').addEventListener('click',function filterPlates(){
+document.getElementById('filterButton').addEventListener('click', function filterPlates() {
     checkReg();
-    var addPlate  = addNumberPlatesFactory(JSON.parse(localStorage.getItem('regArray')));
+    var addPlate = addNumberPlatesFactory(JSON.parse(localStorage.getItem('regArray')));
     var dropDvalue = document.getElementById('townDropD').value;
-    filteredPlate = addPlate.getTown(dropDvalue);
-    displayPlates(filteredPlate);
+    displayPlates(addPlate.filterFunction(dropDvalue));
 });
-
-
--- test.js
+++ test.js
@@ -3,7 +3,6 @@ describe('Numberplates function',function(){
 
     it('should be able to add registrations using the addPlate function',function(){
         //clear the storage for tests to run on a clean sheet
-        localStorage.clear();
         var registrations = ['CA12345','CF12345','CY12345','CJ12345'];
         var addPlate  = addNumberPlatesFactory(registrations);
         addPlate.addPlateElement('cf54321');
@@ -14,7 +13,6 @@ describe('Numberplates function',function(){
     
     it('should return only the registrations form selected towns',function(){
         //clear the storage for tests to run on a clean sheet
-        localStorage.clear();
         var registrations = ['CA12345','CA565656','CF12345','CY12345','CJ12345'];
         var addPlate  = addNumberPlatesFactory(registrations);
         
